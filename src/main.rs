7e81403e6c12567ef71f7b6cdae60bb8e871882d
Iwan BK <iwanbk@gmail.com>
2024-09-11 16:43:38 +0700

    call

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[link(name = "winmycelium.dll", kind = "dylib")]
 extern "C" {
     fn generate_secret_key() -> Vec<u8>;
+    fn start_mycelium(peers: Vec<String>, priv_key: Vec<u8>);
 }
 fn main() {
     unsafe {
-        generate_secret_key();
+        let key = generate_secret_key();
+        let peers = vec!["tcp://185.69.166.7:9651".to_string()];
+        start_mycelium(peers, key);
     }
 }
